9536c6f1b28cf6dd28078e6eb7b087a9b93d40fa
Houssamje <Houssamje00@gmail.com>
2024-04-21 15:36:00 +0100

    ens

---
+++ script/script.js
@@ -0,0 +1,157 @@
+// constants
+const body = document.querySelector("body"),
+    loader = document.querySelector(".loader-wrap"),
+    links = document.querySelectorAll('a[href="#"]'),
+    nav = document.querySelector("header nav"),
+    navToggle = document.querySelector("header nav .toggle"),
+    navSpanMiddle = document.querySelector("header nav .toggle .middle"),
+    navNavigationBar = document.querySelector("header nav .navigation-bar"),
+    navNavigationBarLi = document.querySelectorAll(
+        "header nav .navigation-bar li"
+    ),
+    headerText = document.querySelector("header .text"),
+    headerSection = document.querySelector("header"),
+    aboutSection = document.querySelector(".about-us"),
+    recipeSection = document.querySelector(".recipes"),
+    menuSection = document.querySelector(".menu"),
+    fixedImageSection = document.querySelector(".fixed-image"),
+    footerSection = document.querySelector("footer"),
+    dotOne = document.querySelector(".dots .one"),
+    dotTwo = document.querySelector(".dots .two"),
+    dotThree = document.querySelector(".dots .three"),
+    dots = document.querySelectorAll(".dots > div"),
+    logoImage = document.querySelector("header nav .logo img"),
+    svgDown = document.querySelector("header .arrow-down"),
+    svgUp = document.querySelector(".copyright .arrow-up"),
+    menuImgs = document.querySelectorAll(".menu .menu-image-container img"),
+    boxModel = document.querySelector(".menu .box-model"),
+    menuImageContainer = document.querySelector(".menu-image-container"),
+    boxModelArrow = document.querySelector(".menu .box-model .arrow"),
+    boxModelImage = document.querySelector(".menu .box-model img"),
+    pageTitle = document.querySelector("title");
+
+// remove loader
+function fadeOutEffect() {
+    const fadeEffect = setInterval(function() {
+        if (!loader.style.opacity) {
+            loader.style.opacity = 1;
+        }
+        if (loader.style.opacity > 0) {
+            loader.style.opacity -= 0.4;
+        } else {
+            body.classList.remove('stop-scroll');
+            loader.classList.add('remove');
+            clearInterval(fadeEffect);
+        }
+    }, 100);
+}
+window.addEventListener("load", fadeOutEffect);
+
+// prevent links click hash
+links.forEach(link =>
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+    })
+);
+
+// toggle hamburger menu button
+navToggle.addEventListener("click", () => {
+    navToggle.classList.toggle("active");
+    navSpanMiddle.classList.toggle("hide");
+    navNavigationBar.classList.toggle("show");
+});
+
+// show active navigationbar li
+navNavigationBarLi.forEach(li =>
+    li.addEventListener("click", () => {
+        const arr = Array.from(li.parentElement.children);
+        arr.forEach(li => li.classList.remove("active"));
+        li.classList.add("active");
+    })
+);
+
+// svg-up smooth scroll
+svgUp.addEventListener("click", () => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+var swiper = new Swiper(".home-langage-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 2,
+            
+        },
+        991: {
+            slidesPerView: 3,
+          
+        },
+    },
+});
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+  const form = document.getElementById('form');
+  const result = document.getElementById('result');
+  
+  form.addEventListener('submit', function(e) {
+   
+
+ 
+      const formData = new FormData(form);
+      e.preventDefault();
+  
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+  
+      result.innerHTML = "Please wait..."
+  
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: json
+          })
+          .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                  result.innerHTML = json.message;
+              } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+              }
+          })
+          .catch(error => {
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+          })
+          .then(function() {
+              form.reset();
+              setTimeout(() => {
+                  result.style.display = "none";
+              }, 3000);
+          });
+  });
+  
